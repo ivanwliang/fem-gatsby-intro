a3baf1a118353384b4c7ace5424af581b07efb82
Ivan Liang <ivanwenliang@gmail.com>
2019-11-03 17:09:25 -0800

    Style up hero component

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,15 +4,49 @@ import { Link, graphql, useStaticQuery } from 'gatsby';
 
 const ImageBackground = styled('div')`
   background-image: url('/images/alex-loup-leaves.jpg');
+  background-position: top 20% center;
+  background-size: cover;
+  height: 40vh;
+
+  + * {
+    margin-top: 0;
+  }
+`;
+
+const TextBox = styled('div')`
+  background-image: linear-gradient(to top, #267169dd 2rem, #ddbbff00);
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: flex-end;
+  padding: 0 calc((100vw - 550px) / 2) 2rem;
+  width: 100%;
+
+  h1 {
+    text-shadow: 1px 1px 3px #eeddff66;
+    font-size: 2.25rem;
+  }
+
+  p,
+  a {
+    color: #222;
+    margin-top: 0;
+  }
+
+  a {
+    margin-top: 0.5rem;
+  }
 `;
 
 const Hero = () => {
   return (
     <ImageBackground>
-      <h1>Frontend Masters + Gatsby &hearts;</h1>
-      <p>
-        Hello World <Link to="/about">Learn about me &rarr;</Link>
-      </p>
+      <TextBox>
+        <h1>Frontend Masters + Gatsby &hearts;</h1>
+        <p>
+          Hello World <Link to="/about">Learn about me &rarr;</Link>
+        </p>
+      </TextBox>
     </ImageBackground>
   );
 };
